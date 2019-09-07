ae3f042381fbec960a4290c7d740a06548bf4759
nsh-bed <bed.nsh.web@gmail.com>
2019-09-07 10:52:43 +0900

    MVC 패턴으로 각 메서드 연결시켜서 list.jsp 띄우기, Mybatis 속성값 추가 및 select구문 추가

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -1,8 +1,15 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.dto.Article;
 import com.example.demo.service.ArticleService;
 
 
@@ -10,4 +17,12 @@ import com.example.demo.service.ArticleService;
 public class ArticleController {
 	@Autowired
 	private ArticleService articleService;
+	
+	@RequestMapping("article/list")
+	public String showList(Model model, @RequestParam Map<String, Object> param ) {
+		List<Article> list = articleService.getArticleList(param);
+		model.addAttribute("list", list);
+		
+		return "article/list";
+	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/article/list.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<c:set var="title" value="리스트 페이지"/>
+<%@ include file="../part/head.jspf" %>
+<h1>${title}</h1>
+<table>
+	<tr>
+		<th>번호</th>
+		<th>날짜</th>
+		<th>제목</th>
+		<th>작성자</th>
+		<th>비고</th>
+	</tr>
+	<c:forEach items="${list}" var="article">
+		<tr>
+			<td>${article.id}</td>
+			<td>${article.regDate}</td>
+			<td>${article.title}</td>
+			<td>${article.extra.writer}</td>
+		</tr>
+	</c:forEach>
+</table>
+<%@ include file="../part/foot.jspf" %>
